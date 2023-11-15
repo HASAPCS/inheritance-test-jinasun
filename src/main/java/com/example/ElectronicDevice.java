46314e4b20e6d7f3d2fef78ec1eef918be0441ec
jinasun <ginasuns60916@gmail.com>
2023-11-15 10:35:34 +0800

    nah

--- src/main/java/com/example/ElectronicDevice.java
+++ src/main/java/com/example/ElectronicDevice.java
@@ -1,13 +1,25 @@
 package com.example;
 
 public class ElectronicDevice {
+    private String brand;
+    private String model;
     // TODO: Declare common attributes for electronic devices, such as brand and model.
 
     // Constructor to initialize an ElectronicDevice object
     public ElectronicDevice(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+
         // TODO: Initialize the common attributes (brand, model) with the provided parameters
     }
 
+    public getBrand(){
+
+    }
+
+    public getModel(){
+
+    }
     // TODO: Implement getters (and setters if necessary) for the brand and model.
 
     // OPTIONALLY implement the required methods to pass the BonusTest.
--- src/main/java/com/example/Smartphone.java
+++ src/main/java/com/example/Smartphone.java
@@ -4,12 +4,16 @@ package com.example;
 
 public class Smartphone {
     // TODO: Declare the necessary attributes (brand, model, batteryLevel)
-
+    private int batteryLevel;
     // Constructor to initialize the Smartphone object
     public Smartphone(String brand, String model, int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+
+        
         // TODO: Initialize the attributes with the provided parameters
     }
 
+    // checkBattery
     // TODO: Implement a method to check the battery level and return a message based on the level
     // Implement the checkBattery method to return:
         // "Warning: Battery low!" if the battery level is 20% or lower.
@@ -20,3 +24,7 @@ public class Smartphone {
     // OPTIONALLY implement the required methods to pass the BonusTest.
 
 }
+
+// if batteryyLevel <=20 println ("Warning: Battery low!")
+
+// if batteryLevel > 20 println ("Battery level is okay: [batteryLevel]%.")
